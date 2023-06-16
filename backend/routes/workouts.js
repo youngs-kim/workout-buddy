9a3d05d2d4152b2e98cfe4e24931099dc6af32f7
Youngs-kim <doppio0518@gmail.com>
2023-06-15 17:42:24 -0700

    commiting to push to git

---
+++ backend/routes/workouts.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const {
+  getWorkout,
+  getWorkouts,
+  createWorkout,
+  deleteWorkout,
+  updateWorkout,
+} = require('../controllers/workoutController');
+
+const router = express.Router();
+
+//GET all workouts
+router.get('/', getWorkouts);
+
+//GET a single workouts
+router.get('/:id', getWorkout);
+
+//POST a new workout
+router.post('/', createWorkout);
+
+//DELETE a workout
+router.delete('/:id', deleteWorkout);
+
+//UPDATE a new workout
+router.patch('/:id', updateWorkout);
+
+module.exports = router;
